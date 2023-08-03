f260df33dfaf777d3d28eef575ed252b199cef10
Brenda Jaras <brendajaras@gmail.com>
2023-08-03 18:37:11 -0300

    Modificacion en function.py

--- function.py
+++ function.py
@@ -3,7 +3,7 @@ from collections import Counter
 import ast
 
 genres = pd.read_csv('Data\genres.csv')
-genres.genres = genres.genres.apply(ast.literal_eval)
+genres[genres] = genres[genres].apply(ast.literal_eval)
 names = pd.read_csv('Data\\names.csv')
 specs_df = pd.read_csv('Data\specs.csv')
 specs_df.specs = specs_df.specs.apply(ast.literal_eval)
